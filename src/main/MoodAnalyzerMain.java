807f50a7deb982a084aa7ed356e6186eb91ba9c3
shilpa123 <shilpabiradar100@gmail.com>
2023-03-28 15:32:55 +0530

    Added : MoodAnalysis exception for null and empty

---
+++ src/main/MoodAnalyzerMain.java
@@ -0,0 +1,33 @@
+package main;
+
+public class MoodAnalyzerMain {
+    private String message;
+    /**
+     * create a default constructor name as MoodAnalyzerMain
+     */
+    public MoodAnalyzerMain() {
+    }
+
+    public MoodAnalyzerMain(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            else if (message.contains("")){
+                throw new MoodAnalysisException(MoodAnalysisException.Exception_Type.EMPTY, "Please provide valid mood, dont keep empty in arguments");
+            }
+            else
+                return "HAPPY";
+        }
+        catch (Exception e) {
+            if (this.message == null) {
+                throw new MoodAnalysisException(MoodAnalysisException.Exception_Type.NULL, "Please provide valid mood, dont provide null in arguments");
+            }
+            else
+                throw new MoodAnalysisException(MoodAnalysisException.Exception_Type.EMPTY, "Please provide valid mood, dont keep empty in arguments");
+        }
+    }
+}
---
+++ src/test/MoodAnalyserTest.java
@@ -0,0 +1,27 @@
+package test;
+
+
+
+import main.MoodAnalysisException;
+import main.MoodAnalyzerMain;
+import org.junit.jupiter.api.*;
+public class MoodAnalyserTest {
+    MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain(null);
+
+    //TC 3.1 Given NULL Mood Should Throw MoodAnalysisException
+    @Test
+    public void testMoodAnalysis1() throws MoodAnalysisException {
+        String mood = moodAnalyzer.analyseMood();
+        Assertions.assertEquals(mood, "HAPPY");
+    }
+
+    //Given Empty Mood Should Throw MoodAnalysisException indicating Empty
+
+    MoodAnalyzerMain moodAnalyzer2 = new MoodAnalyzerMain("");
+    @Test
+    public void testMoodAnalysis2() throws MoodAnalysisException {
+        String mood2 = moodAnalyzer2.analyseMood();
+        System.out.println(mood2);
+        Assertions.assertEquals(mood2, "HAPPY");
+    }
+}
